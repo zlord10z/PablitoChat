2c0d0cc7899d6c23e939935285ca6b7a98ab9d91
zlord10z <64948473+zlord10z@users.noreply.github.com>
2021-10-15 14:41:27 +0200

    Update pablitoChat.py
    -Multicast available on Windows
    -change tk to Toplevel at preferences window

--- pablitoChat.py
+++ pablitoChat.py
@@ -83,7 +83,8 @@ def searchServers():
     
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind((MCAST_GRP, MCAST_PORT))
+    if sys.platform ==  'win32': sock.bind(('',MCAST_PORT)) ##zmiana
+    else: sock.bind((MCAST_GRP, MCAST_PORT))  
     mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
@@ -182,7 +183,7 @@ def add_room():
     print("x")
 
 def preferences():
-    window = tk.TK()
+    window = tk.Toplevel()
     window.geometry("350x400")
     window.configure(background='blue')
     window.title("Opcje")
